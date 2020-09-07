15502dd8fbdcbf5394bb6f7009202f07fffe5287
hanyuanlin <690051834@qq.com>
2020-09-07 18:06:24 +0800

    fix: eslint不检测qiankun注入的windows变量

--- src/shared/http.js
+++ src/shared/http.js
@@ -4,6 +4,7 @@ const packageName = require("../../package.json").name;
 
 // 如果作为微应用，应该在加上该应用的标识前缀，以区分其他应用的接口，并且让主应用能够正确代理到微应用中，再从微应用代理到服务器（并且通过pathRewrite去掉该标识前缀）
 let baseURL = "/api";
+// eslint-disable-next-line no-constant-condition
 if (window.__POWERED_BY_QIANKUN__ || true) {
   baseURL += `/${packageName}`;
 }
