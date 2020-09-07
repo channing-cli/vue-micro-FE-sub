0fa08beeb99b4f283149c1108117b1c3aa76def7
hanyuanlin <690051834@qq.com>
2020-09-07 18:14:09 +0800

    fix: moment的filter中使用new Date()方式包裹参数消除警告

--- src/filter/index.js
+++ src/filter/index.js
@@ -2,5 +2,5 @@ import Vue from "vue";
 import moment from "moment";
 
 Vue.filter("formatDate", (val, formatStr) => {
-  return val ? moment(val).format(formatStr) : "--";
+  return val ? moment(new Date(val)).format(formatStr) : "--";
 });
